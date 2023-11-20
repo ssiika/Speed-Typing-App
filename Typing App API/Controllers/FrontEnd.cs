dbe0ef078957a169885d0c0f7dbdf0b61828d981
justinnyilas <justin.nyilas@nodescribe.com>
2023-11-20 16:38:57 +1100

    Add front end build folder to wwwroot folder on backend, enable serving static files from back end

---
+++ Typing App API/Controllers/FrontEnd.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Typing_App_API.Controllers
+{
+    public class FrontEnd : Controller
+    {
+        [Route("")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return File("~/index.html", "text/html");
+        }
+    }
+}
