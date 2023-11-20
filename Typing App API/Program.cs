37ea232c20f1934a6566fd37cca67586f45df855
justinnyilas <justin.nyilas@nodescribe.com>
2023-11-20 22:30:35 +1100

    Add EnableRetryOnFailure to UseSqlServer in program.cs, adjust css on register page

--- Typing App API/Program.cs
+++ Typing App API/Program.cs
@@ -21,7 +21,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddDbContext<DataContext>(options => 
-    options.UseSqlServer(Environment.GetEnvironmentVariable("DefaultConnection")));
+    options.UseSqlServer(Environment.GetEnvironmentVariable("DefaultConnection"),
+    providerOptions => providerOptions.EnableRetryOnFailure()));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
